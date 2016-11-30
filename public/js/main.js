670e5bc298b1c94b9af3a605f350439f6292a8a0
Francisco Ruiz Mtz <ismael_rmtz@outlook.com>
2016-11-30 12:09:53 -0600

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#books').pinterest_grid({
+    no_columns: 4,
+    padding_x: 10,
+    padding_y: 10,
+    margin_bottom: 50,
+    single_column_breakpoint: 700
+  });
+
+  //update item cart
+  $(".btn-update-item").on('click', function(e){
+    e.preventDefault();
+    var id = $(this).data('id');
+    var href = $(this).data('href');
+    var quantify = $("#book_" + id).val();
+
+    window.location.href = href + "/" + quantify;
+  });
+
+});
